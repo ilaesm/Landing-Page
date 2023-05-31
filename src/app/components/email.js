22d372099c0fbb3dd6e27b348dec471f2296415a
Ila Esmaeili <ilaesm@Ilas-MacBook-Pro.local>
2023-05-31 14:34:03 -0700

    hero section

--- src/app/components/email.js
+++ src/app/components/email.js
@@ -2,19 +2,10 @@ import React from 'react'
 
 export default function EmailForm() {
   return (
-<form action="#">
-      <div class="relative">
-        <label for="email" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
-        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-          <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-        </svg>
-      </div>
-      <input class="block p-3 pl-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" required="" />
-      <div>
-        <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
-      </div>
-      <div class="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">We care about the protection of your data. <a href="#" class="font-medium text-primary-600 dark:text-primary-500 hover:underline">Read our Privacy Policy</a>.</div>
-    </form>
+
+<div className="flex flex-row">
+			<input type="text" placeholder="example@email.com" className="w-3/5 p-3 rounded-l-lg sm:w-2/3" />
+			<button type="button" className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 dark:bg-violet-400 dark:text-gray-900">Subscribe</button>
+		</div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,16 @@
+
 'use client';
 import React from 'react';
-import Image from 'next/image'
-import Spline from '@splinetool/react-spline';
-import LandingHead from './components/header';
-import EmailForm from './components/email';
-import Datasource from './components/data';
+import HeroSection from './components/Hero';
+import FooterSection from './components/footer';
 
-export default function Home() {
+
+export default function page() {
   return (
-<div className="max-w-[75%] w-full items-center mx-auto ">
-  <LandingHead />
-  <section className="bg-white dark:bg-gray-900">
-    <div className="grid px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-      <div className="mr-auto place-self-center lg:col-span-7">
-        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Heading Heading Heading</h1>
-        <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">desc desc desc desc</p>
-        <EmailForm />
-      </div>
-      <div className="hidden lg:mt-0 lg:col-span-5 lg:flex ml-8">
-        <SplineCustom scene="https://prod.spline.design/18tE3AOOB4u-iAA2/scene.splinecode" />
-      </div>
+    <div class="max-w-[92%] mx-auto px-4 sm:px-6 lg:px-8">
+    <HeroSection />
+    <FooterSection />
     </div>
-  </section>
-  <Datasource />
-</div>
-
-  );
-}
-
-function SplineCustom({ scene }) {
-  const style = {
-    width: '350px',
-    height: '350px',
-  };
-
-  return (
-    <div style={style}>
-      <Spline scene={scene} />
-    </div>
-  );
+    
+  )
 }
